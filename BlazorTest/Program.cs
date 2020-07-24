cdaaf1d7fd1b5e34f33f06c6a4eed6b6baf4a349
anchery <35939871+anchery@users.noreply.github.com>
2020-07-24 13:00:35 -0500

    Add project files.

---
+++ BlazorTest/Program.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace BlazorTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            string log = config.GetSection("Logging").GetValue<string>("PathFormat");
+            log += string.Concat("\\log-",DateTime.Now.ToString("yyyyMMdd"), ".txt");
+            
+            Log.Logger = new LoggerConfiguration()
+            .Enrich.FromLogContext()
+            .WriteTo.File(log)
+
+            //.WriteTo.File(@"C:\Scantron\log-20200724.txt")
+            .CreateLogger();
+
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                //.ConfigureLogging((hostingContext, builder) =>
+                //{
+                //    var config = new ConfigurationBuilder()
+                //    .AddJsonFile("appsettings.json")
+                //    .Build();
+
+                //    string log = config.GetSection("Logging").GetValue<string>("PathFormat");
+                //    builder.AddFile(log);
+                //})
+            .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
